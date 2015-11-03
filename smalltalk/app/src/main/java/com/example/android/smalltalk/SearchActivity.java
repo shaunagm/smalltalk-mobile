759ec6f9eee5871a50782bbdfc6c065bde71cb7c
Shauna <shaunagm@gmail.com>
2015-11-03 18:34:23 -0500

    Prettify search activity

--- smalltalk/app/src/main/java/com/example/android/smalltalk/SearchActivity.java
+++ smalltalk/app/src/main/java/com/example/android/smalltalk/SearchActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,6 +19,10 @@ import com.example.android.smalltalk.SmalltalkUtilities.misc_utils;
 import com.example.android.smalltalk.data.ObjectCursorAdapter;
 
 public class SearchActivity extends BaseActivity {
+
+    String[] query_type = new String[]{"topics", "contacts", "groups"};
+    int[] type_ids = new int[]{ R.id.search_topic_objects,  R.id.search_contact_objects,
+            R.id.search_group_objects};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,34 +33,14 @@ public class SearchActivity extends BaseActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
 
             String query = intent.getStringExtra(SearchManager.QUERY);
-            String[] query_type = new String[]{"topics", "contacts", "groups"};
 
-            for (String type: query_type) {
-                final String final_type = type;
+            for(int l=0; l<=2; l++) {  // Loop through three query types
+
+                final String type = query_type[l];
                 Cursor cursor = db_utils.searchQuery(this, type, query);
+                ListView object_list = (ListView) findViewById(type_ids[l]);
+
                 if (cursor.getCount() > 0) {
-                    int header_id = 0;
-                    int objects_id = 0;
-                    switch (type) {
-                        case "topics":
-                            header_id = R.id.search_contacts_header;
-                            objects_id = R.id.search_contact_objects;
-                            break;
-                        case "contacts":
-                            header_id = R.id.search_topics_header;
-                            objects_id = R.id.search_topic_objects;
-                            break;
-                        case "groups":
-                            header_id = R.id.search_groups_header;
-                            objects_id = R.id.search_group_objects;
-                            break;
-                    }
-
-                    TextView header = (TextView) findViewById(header_id);
-                    header.setVisibility(View.VISIBLE);
-
-                    ListView object_list = (ListView) findViewById(objects_id);
-                    object_list.setVisibility(View.VISIBLE);
 
                     final ObjectCursorAdapter adapter = new ObjectCursorAdapter(this, cursor, 0);
                     object_list.setAdapter(adapter);
@@ -63,12 +48,56 @@ public class SearchActivity extends BaseActivity {
                         @Override
                         public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                             Cursor row_cursor = (Cursor) adapter.getItem(position);
-                            misc_utils.goToDetailView(view.getContext(), row_cursor, final_type);
+                            misc_utils.goToDetailView(view.getContext(), row_cursor, type.replace("s",""));
                         }
                     });
+
                 }
+
+            }
+
+            // Buttons!
+            Button show_topics = (Button) findViewById(R.id.search_show_topics);
+            show_topics.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showType(0);
+                }
+            });
+            Button show_contacts = (Button) findViewById(R.id.search_show_contacts);
+            show_contacts.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showType(1);
+                }
+            });
+            Button show_groups = (Button) findViewById(R.id.search_show_groups);
+            show_groups.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showType(2);
+                }
+            });
+
+
+
+        }
+    }
+
+    public void showType(int position) {
+
+        TextView header = (TextView) findViewById(R.id.searchview_header);
+        header.setText(query_type[position]);
+
+        for (int id:type_ids) {
+            ListView object_list = (ListView) findViewById(id);
+            if (id == type_ids[position]) {
+                object_list.setVisibility(View.VISIBLE);
+            } else {
+                object_list.setVisibility(View.GONE);
             }
         }
+
     }
 
 }
