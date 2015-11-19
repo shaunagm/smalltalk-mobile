aa866defb6a54891c878de77d5ada3c477607a5c
shaunagm <shaunagm@gmail.com>
2015-11-19 16:19:18 -0500

    Add function in misc_utils for adapting display to different screens

--- smalltalk/app/src/main/java/com/example/android/smalltalk/SmalltalkUtilities/misc_utils.java
+++ smalltalk/app/src/main/java/com/example/android/smalltalk/SmalltalkUtilities/misc_utils.java
@@ -2,9 +2,12 @@ package com.example.android.smalltalk.SmalltalkUtilities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.database.Cursor;
 
 import com.example.android.smalltalk.DetailActivity;
+
+import java.util.HashMap;
 
 /**
  * Created by shauna on 9/7/15.
@@ -18,5 +21,61 @@ public class misc_utils {
                 .putExtra("item_type", item_type.toLowerCase());
         context.startActivity(intent);
     }
+
+    public static HashMap getScreenAdaption(Context context) {
+        HashMap screenAdaptationHashMap = new HashMap();
+
+        int size = context.getResources().getConfiguration().screenLayout;
+        int layout = context.getResources().getConfiguration().orientation;
+
+        switch (size) {
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                if (layout == Configuration.ORIENTATION_LANDSCAPE) {
+                    screenAdaptationHashMap.put("dualPane", true);
+                    screenAdaptationHashMap.put("extraDetail", false);
+                    screenAdaptationHashMap.put("customRelationship", false);
+                } else {
+                    screenAdaptationHashMap.put("dualPane", false);
+                    screenAdaptationHashMap.put("extraDetail", false);
+                    screenAdaptationHashMap.put("customRelationship", false);
+                }
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_LARGE:
+                if (layout == Configuration.ORIENTATION_LANDSCAPE) {
+                    screenAdaptationHashMap.put("dualPane", true);
+                    screenAdaptationHashMap.put("extraDetail", true);
+                    screenAdaptationHashMap.put("customRelationship", true);
+                } else {
+                    screenAdaptationHashMap.put("dualPane", true);
+                    screenAdaptationHashMap.put("extraDetail", false);
+                    screenAdaptationHashMap.put("customRelationship", false);
+                }
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+                if (layout == Configuration.ORIENTATION_LANDSCAPE) {
+                    screenAdaptationHashMap.put("dualPane", true);
+                    screenAdaptationHashMap.put("extraDetail", true);
+                    screenAdaptationHashMap.put("customRelationship", true);
+                } else {
+                    screenAdaptationHashMap.put("dualPane", false);
+                    screenAdaptationHashMap.put("extraDetail", true);
+                    screenAdaptationHashMap.put("customRelationship", true);
+                }
+                break;
+            default:  // Normal
+                if (layout == Configuration.ORIENTATION_LANDSCAPE) {
+                    screenAdaptationHashMap.put("dualPane", true);
+                    screenAdaptationHashMap.put("extraDetail", true);
+                    screenAdaptationHashMap.put("customRelationship", true);
+                } else {
+                    screenAdaptationHashMap.put("dualPane", false);
+                    screenAdaptationHashMap.put("extraDetail", false);
+                    screenAdaptationHashMap.put("customRelationship", false);
+                }
+        }
+
+        return screenAdaptationHashMap;
+    }
+
 
 }
